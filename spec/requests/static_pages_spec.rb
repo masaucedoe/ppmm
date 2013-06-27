11218cfcd4b07c85fdd9a8773d962104c976a70f
Mario Saucedo <m.a.saucedo.e@gmail.com>
2013-06-26 23:51:09 -0500

    Filling Layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,25 +6,25 @@ describe "Static pages" do
 	let(:base_title) { "Aplicación PPMM" }
 
 	describe "Inicio page" do
-		before { visit '/static_pages/inicio' }
+		before { visit root_path }
 			it { should have_selector('h1', text: 'Aplicación PPMM') } # Page should have the content 'Aplicación PPMM'.
 			it { should have_selector('title', text: "#{base_title} | Inicio") } # Page should have the title 'Inicio'.
 	end
 
 	describe "Ayuda page" do
-		before { visit '/static_pages/ayuda' }
+		before { visit ayuda_path }
 			it { should have_selector('h1', text: 'Ayuda') } # Page should have the content 'Ayuda'.
 			it { should have_selector('title', text: "#{base_title} | Ayuda") } # Page should have the title 'Ayuda'.
 	end
 
 	describe "Empresa page" do
-		before { visit '/static_pages/empresa' }
+		before { visit empresa_path }
 			it { should have_selector('h1', text: 'Nuestra Empresa') } # Page should have the content 'Nuestra Empresa'.
 			it { should have_selector('title', text: "#{base_title} | Acerca de nosotros") } # Page should have the title 'Acerca de nosotros'.
 	end
 
 	describe "Contacto page" do
-		before { visit '/static_pages/contacto' }
+		before { visit contacto_path }
 			it { should have_selector('h1', text: 'Contáctanos') } # Page should have the content 'Contáctanos'.
 			it { should have_selector('title', text: "#{base_title} | Contacto") } # Page should have the title 'Contacto'.
 	end
